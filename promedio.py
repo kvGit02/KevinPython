35f7ff60fc72d67e2d497fbdacbac5514e3e0298
kvGit02 <dowoss98@gmail.com>
2026-04-28 09:18:41 -0400

    rt

---
+++ promedio.py
@@ -0,0 +1,9 @@
+nota1=float(input("Ingrese la primera nota: "))
+nota2=float(input("Ingrese la segunda nota: "))
+nota3=float(input("Ingrese la tercera nota: "))
+prom=(nota1+nota2+nota3)/3
+if prom>=4.0:
+    print("Aprobo")
+else:
+    print("No aprobo")
+   
