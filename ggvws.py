068bc4f487c74758a7a98f56d58fe757f9cc403f
kvGit02 <dowoss98@gmail.com>
2026-05-26 09:53:56 -0400

    4ferww

---
+++ ggvws.py
@@ -0,0 +1,14 @@
+
+print("Umbral")
+print("--------------------------------")
+
+nivel_numerico = 0
+
+
+nivel_numerico = int(input("Ingrese el nivel numerico del pais:"))
+
+
+if nivel_numerico <= 40:
+    print("Pais en vias de desarrollo")
+else:
+    print("Pais desarrollado")
