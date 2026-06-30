659a3b59160ca1058bd614232a37177c0e836048
kvGit02 <dowoss98@gmail.com>
2026-06-30 09:37:49 -0400

    fefws

--- diccionarios/diccio.py
+++ diccionarios/diccio.py
@@ -4,7 +4,8 @@ autos = {
     "A002" : ["Ford", "Ranger", 2019,4],
     "A003" : ["Chevrolet", "Spark", 2022,4],
     "A004" : ["Suzuki", "Aerio", 2005,4],
-    "A005" : ["Toyota", "Yaris", 20015,5]
+    "A005" : ["Toyota", "Yaris", 20015,5],
+    "A006" : ["Chevrolet", "Impala", 1950,1]
 }
 
 operaciones = {
@@ -13,8 +14,9 @@ operaciones = {
     "A002" : ["07-08-2024", "01-08-2025"],
     "A003" : ["09-01-2025", "Pendiente"],
     "A004" : ["24-03-2025", "Pendiente"],
-    "A005" : ["24-03-2024", "24-07-2024"]
-
+    "A005" : ["24-03-2024", "24-07-2024"],
+    "A006" : ["24-03-2024", "24-09-2024"]
+                #0           #1
 
 }
 
@@ -22,9 +24,14 @@ def autos_vendidos_por_marca(marca):
     total = 0
 
     for id_auto, datos in autos.items():# ".items" para expulsar
-        print(id_auto, datos)
-        print(datos[0])
+        # print(id_auto, datos)
+        # print(datos[0])
+        if datos[0].lower() == marca.lower():
+            if operaciones[id_auto][1] != "Pendiente":
+                total +=1
+        
+    print(f"El numero total de autos vendidos de la marca {marca.upper()} es {total}")
 
 #main
-
-autos_vendidos_por_marca("Toyota")
+marca = input("Ingrese la marca a buscar: ")
+autos_vendidos_por_marca(marca)
